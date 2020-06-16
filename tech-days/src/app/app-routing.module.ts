beead535a6090dd64767a8bff793fd27d5a04bbd
RutujaSatkar96 <60915608+RutujaSatkar96@users.noreply.github.com>
2020-06-16 13:10:37 +0530

    added assets

--- tech-days/src/app/app-routing.module.ts
+++ tech-days/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuoteComponent } from './quote/quote.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'app-quote', pathMatch: 'full'},
+  { path: 'app-quote', component: QuoteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
