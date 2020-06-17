684025ee720a86154464e0d675db3e50a58e3054
RutujaSatkar96 <syntel@syncgdc3833.syntelorg.com>
2020-06-17 05:43:57 +0530

    added sidebar component

--- tech-days/src/app/app.module.ts
+++ tech-days/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    DashboardComponent
+    DashboardComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
