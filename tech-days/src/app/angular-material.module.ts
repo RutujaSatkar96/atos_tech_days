2bdde6c03fb4bfa00efedc6187b616dca4bca54e
RutujaSatkar96 <60915608+RutujaSatkar96@users.noreply.github.com>
2020-06-18 15:35:27 +0530

    added property info screen

--- tech-days/src/app/angular-material.module.ts
+++ tech-days/src/app/angular-material.module.ts
@@ -3,10 +3,12 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @NgModule({
-    imports: [MatTabsModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule],
-    exports: [MatTabsModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule]
+    imports: [MatTabsModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
+    exports: [MatTabsModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
 })
 
 export class AngularMaterialModule { }
